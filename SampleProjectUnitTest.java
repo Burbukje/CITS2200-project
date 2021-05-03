3a1fa83ac50020c078983c63854c6b7c8e0d7881
Bea <35147007+Burbukje@users.noreply.github.com>
2021-05-03 14:20:05 +0800

    Create SampleProjectUnitTest.java

---
+++ SampleProjectUnitTest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * @author Andrew "Gozz" Gozzard <andrew.gozzard@uwa.edu.au>
+ * @author Tom Hill Almeida <00087533@uwa.edu.au>
+ *
+ * A sample unit tester for the 2021 CITS2200 Project.
+ *
+ * DO NOT modify this file in the course of completing your project.
+ * You are, however, encouraged to copy it and use it to construct your own tests.
+ */
+public class SampleProjectUnitTest {
+    /**
+     * Total number of tests run
+     */
+    private static int numTest = 0;
+
+    /**
+     * Total number of tests passed
+     */
+    private static int numPass = 0;
+
+    /**
+     * Helper function to print out a test result nicely and update counters
+     * @param name The name of the test to display as output
+     * @param pass Whether or note the test passed
