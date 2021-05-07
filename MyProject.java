ff2abc670d7d3e1cfecb0295f7be9e54f7a30943
Tristan-ward <Tristan.p.ward@gmail.com>
2021-05-07 17:06:35 +0800

    Commit to change Vector to int

--- MyProject.java
+++ MyProject.java
@@ -1,112 +1,205 @@
 // Tristan Ward (22756187)
+
 import java.util.*;
-import java.util.Graph;
 
 public class MyProject implements Project {
-    
+
     ///////////// Private Methods implemented for modular use ///////////////
 
-    /** 
-    private int getNumberOfVertices(int[][] adjlist){
-        int v = 0;
-        for (int c : adjlist.length) {
-            v++;
-        }
-    }
-    */
+    private int[][] calculateTranspose(int[][] adjlist) {
+        /**
+         * Vector<Vector<Integer>? Vector<Vector<Integer>> adjtranspose = new
+         * Vector<Vector<Integer>>(); ; // Max number of edges is equal to (vertices -
+         * 1)
+         * 
+         * for (int i = 0; i < adjlist.length; i++) { adjtranspose.add(i, new
+         * Vector<Integer>()); }
+         * 
+         * for (int i = 0; i < adjlist.length; i++) { for (int c : adjlist[i]) { // rws
+         * in adjlist become columns in adjtranspose /** Vector<Integer> rowVector =
+         * adjtranspose.get(c); rowVector.add(i);
+         */
+        /**
+         * adjtranspose.get(c).add(i); } }
+         */
+        int[][] adjtranspose = new int[adjlist.length][adjlist.length];
+        ; // Max number of edges is equal to (vertices - 1)
 
-    private Graph calculateTranspose(int[][] adjlist, int v){
-        Graph<Integer> adjtranspose = new Graph<Integer>();
-        for (int i=0; i < v; i++)
-        {
-            for (int c : adjlist[i])
-            {
-                adjtranspose.connect(c,i);
+        for (int i = 0; i < adjlist.length; i++) {
+            for (int j = 0; j < adjlist.length; j++) {
+                adjtranspose[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < adjlist.length; i++) {
+            for (int c : adjlist[i]) { // rws in adjlist become columns in adjtranspose
+                /**
+                 * Vector<Integer> rowVector = adjtranspose.get(c); rowVector.add(i);
+                 */
+
+                adjtranspose[c][i] = 1;
             }
         }
         return adjtranspose;
     }
- 
 
-   ///////////// Public methods of Project  ////////////////////////////////////
+    private class Edge implements Comparable<Edge> {
+        private int vertex;
+        private int weight;
 
+        public Edge(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+
+        }
+
+        public int getVertex() {
+            return vertex;
+        }
+
+        public void setVertex(int vertex) {
+            this.vertex = vertex;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            Edge edge = (Edge) o;
+            return Integer.compare(weight, edge.weight) == 0 && Integer.compare(this.vertex, edge.getVertex()) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(vertex, weight);
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" + "vertex='" + vertex + '\'' + ", weight=" + weight + '}';
+        }
+
+        // Compare two edge objects by their weight
+        @Override
+        public int compareTo(Edge edge) {
+            if (this.getWeight() > edge.getWeight()) {
+                return 1;
+            } else if (this.getWeight() < edge.getWeight()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    ///////////// Public methods of Project ////////////////////////////////////
 
     public boolean allDevicesConnected(int[][] adjlist) {
         // TODO
-        //Return: boolean indicating whether all of the devices in the network are connected to the network
-        for(int i = 0; i<adjlist.length; i++){
-            for(int j = 0; j <adjlist.length; j++){
-                if(i!=j && adjlist[i][j]==0){ 
-                    //Hi
-                    return false;
-                }
-            
-        }
+        // Return: boolean indicating whether all of the devices in the network are
+        // connected to the network
+        /**
+         * for (int i = 0; i < adjlist.length; i++) { for (int j = 0; j <
+         * adjlist.length; j++) { if (i != j && adjlist[i][j] == 0) { // Hi return
+         * false; } } }
+         */
         return true;
-    }
     }
 
     public int numPaths(int[][] adjlist, int src, int dst) {
-        v = adjlist.length;
 
-       // get distances from lab 6 to achieve minimun spaning tree from dst
+        // get distances from lab 6 to achieve minimun spaning tree from dst
 
-       // Need to use the transpose of the adjlist as we want the tree that leads to dst. 
-       Graph<Integer> gtranspose = calculateTranspose(adjlist , v);
+        // Need to use the transpose of the adjlist as we want the tree that leads to
+        // dst.
+        Vector<Vector<Integer>> gtranspose = calculateTranspose(adjlist);
+        /**
+         * // BFS implementation LinkedList<Integer> q = new LinkedList<Integer>();
+         * int[] distances = new int[v]; char[] colour = new char[v];
+         * 
+         * for(int c = 0; c < v ; c++){ colour[c] = 'w'; distances[c] = -1; }
+         * 
+         * distances[dst] = 0; colour[dst] = 'g'; q.add( dst );
+         * 
+         * while (!q.isEmpty()){ int w = q.removeFirst(); for (int x : gtranspose[w]){
+         * // might not work as getVerticesConectedTo returns a list and not an array
+         * if(colour[x]== 'w'){ distances[x] = distances[w] +1; //CHECK IF CORRECT. w is
+         * the parent of x so I think it is correct. colour[x] = 'g'; q.addLast(x); } }
+         * colour[w] = 'b'; }
+         */
 
-       // BFS implementation
-        LinkedList<Integer> q = new LinkedList<Integer>();
-		int[] distances = new int[v];
-		char[] colour = new char[v];
+        boolean[] inMST = new boolean[gtranspose.size()];
+        int[] key = new int[gtranspose.size()];
+        int pathWeight;
 
-		for(int c = 0; c < v ; c++){
-			colour[c] = 'w';
-			distances[c] = -1;
-		}
-        
-		distances[dst] = 0;
-		colour[dst] = 'g';
-		q.add( dst );
-		
-		while (!q.isEmpty()){
-			int w = q.removeFirst();
-			for (int x : gtranspose.getVerticesConectedTo(w)){ // might not work as getVerticesConectedTo returns a list and not an array
-				if(colour[x]== 'w'){
-					distances[x] = distances[w] +1; //CHECK IF CORRECT. w is the parent of x so I think it is correct. 
-					colour[x] = 'g';
-					q.addLast(x);
-				}				
-			}
-			colour[w] = 'b';
-		}
+        for (int c = 0; c < gtranspose.size(); c++) {
+            inMST[c] = false;
+            key[c] = -1;
+        }
 
-        // distances stores the shortest distance from dst to all vertices
-        
-        // BFS but only add vertices that have a shorter distance to dst than current vertex.
-        // the queue "q" is empty so we can reuse it 
+        PriorityQueue<Edge> edgePQ = new PriorityQueue<>();
+        edgePQ.add(new Edge(dst, 0));
 
+        while (!edgePQ.isEmpty()) {
+            Edge u = edgePQ.poll();
+            if (inMST[u.getVertex()] == false) {
+                inMST[u.getVertex()] = true;
+                key[u.getVertex()] = u.getWeight();
+
+                for (int v = 0; v < gtranspose.size(); v++) {
+                    // Vector<Integer> rowVector = gtranspose.get(u).get(v) ;
+
+                    if (inMST[v] == false && gtranspose.get(u.getVertex()).get(v) > 0) {
+                        pathWeight = u.getWeight() + gtranspose.get(u.getVertex()).get(v);
+                        edgePQ.add(new Edge(v, pathWeight));
+                    }
+
+                }
+            }
+        }
+
+        // distances stores the shortest distance from all vertices to dst
+
+        // BFS but only add vertices that have a shorter distance to dst than current
+        // vertex.
+        // the queue "q" is empty so we can reuse it
+        int[] colour = new int[adjlist.length];
         int paths = 0;
-
+        LinkedList<Integer> q = new LinkedList<Integer>();
         // colour can also be reused if we set all values to white
-		for(int c = 0; c < v ; c++){
-			colour[c] = 'w';
-		}
-        
 
-		colour[src] = 'g';
-		q.add( src );
-		
-		while (!q.isEmpty()){
-			int w = q.removeFirst();
-			for (int x : adjlist[w]){ 
-                if(x == dst) { path++;}
-				if(colour[x]== 'w'){
-					colour[x] = 'g';
-					if(distances(w) >= distances(x)){ q.addLast(x);}
-				}				
-			}
-			colour[w] = 'b';
-		}
+        for (int c = 0; c < adjlist.length; c++) {
+            colour[c] = 'w';
+        }
+
+        colour[src] = 'g';
+        q.add(src);
+
+        while (!q.isEmpty()) {
+            int w = q.removeFirst();
+            for (int x : adjlist[w]) {
+                if (x == dst) {
+                    paths++;
+                }
+                if (colour[x] == 'w') {
+                    colour[x] = 'g';
+                    if (key[w] >= key[x]) {
+                        q.addLast(x);
+                    }
+                }
+            }
+            colour[w] = 'b';
+        }
 
         return paths;
     }
